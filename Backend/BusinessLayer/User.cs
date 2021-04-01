ebb35f0a28436486e215e39a7b597137ba718f28
shirlavi <shirush40@gmail.com>
2021-04-01 21:52:41 +0300

    add two new classes User and UserCnoroller

--- Backend/BusinessLayer/User.cs
+++ Backend/BusinessLayer/User.cs
@@ -14,14 +14,87 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         private string password;
         private Dictionary<string, Task> TaskList;
         private Dictionary<string, Board> BoardList;
-        public User(string email, string password) { }
-        public bool validatePasswordMatch(string password) { }
-        public void logout() { }
-        public bool addBoard(string Boardid) { }
-        public Board createBoard(string BoardName) { }
-        public Board removeBoard(string BoardID) { }
-        public bool equals(User other) { }
-        public List<Task> getTasks() { }
+        public User(string email, string password) 
+        {
+            BoardController BoardController = new BoardController(); //צריך לבנות בנאי ריק
+            isLogged = false;
+            this.email = email;
+            this.password = password;
+            Dictionary<string, Task> TaskList = new Dictionary<string, Task>();
+            Dictionary<string, Board> BoardList = new Dictionary<string, Board>();
+        }
+        public bool validatePasswordMatch(string password) 
+        {
+            return password.Equals(this.password);
+        }
+        public void logout() 
+        {
+            this.setIslogged(false);
+        }
+        public bool addBoard(Board board) 
+        {
+            if (BoardList.ContainsKey(board.getId()) == true) //צריך לכתוב getID
+            {
+                Console.WriteLine("this board is already excist");
+                return false;
+            }
+            else
+            {
+                BoardList.Add(board.getId(), board); 
+                return true;
+            }
+        }
+        public Board createBoard(string BoardName) 
+        {
+            Board board = new Board(BoardName, this.email);
+            this.BoardController.addBoard(board);
+            return board;
+        }
+        public Board removeBoard(string idBoard) 
+        {
+            if (BoardList.ContainsKey(idBoard) == true) 
+            {
+                Board removed = getBoard(idBoard);
+                BoardList.Remove(idBoard);
+                return removed;
+            }
+            else
+            {
+                throw new ArgumentException("this board is not excist");
+            }
+        }
+        public bool equals(User other)
+        {
+            return this.email.Equals(other.email);
+        }
+
+        public Board getBoard(string idBoard)
+        {
+            if (BoardList.ContainsKey(idBoard) == true)
+            {
+                return BoardList[idBoard];
+            }
+            else
+            {
+                throw new ArgumentException("there is no board with this id");
+            }
+        }
+        public Task getTasks(string idTask)
+        {
+            if (TaskList.ContainsKey(idTask) == true)
+            {
+                return TaskList[idTask];
+            }
+            else
+            {
+                throw new ArgumentException("there is no task with this id");
+            }
+        }
+
+        public void setIslogged(bool logged)
+        {
+            isLogged = logged;
+        }
 
     }
 }
--- Backend/BusinessLayer/UserController.cs
+++ Backend/BusinessLayer/UserController.cs
@@ -8,13 +8,74 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 {
     class UserController
     {
-        private Dictionary<string, User> Users;
-        public UserController() { }
-        public User getUser(string email) { }
-        public User login(string email, string password) { }
-        public User register(string email, string password) { }
-        private bool validateEmail(string email) { }
-        private bool validatePasswordRules(string password) { }
+        private Dictionary<string, User> users;
+        public UserController() 
+        {
+            Dictionary<string, User> users = new Dictionary<string, User>();
+        }
+        public User getUser(string email)
+        {
+            if (users.ContainsKey(email) == true)
+            {
+                return users[email];
+            }
+            else
+            {
+                throw new ArgumentException("there is no user with this email");
+            }
+        }
+        public User login(string email, string password)
+        {
+            if (users.ContainsKey(email) == true)
+            {
+                if (this.getUser(email).validatePasswordMatch(password))
+                {
+                    this.getUser(email).setIslogged(true);
+                    return this.getUser(email);
+                }
+                throw new ArgumentException("the password is not match");
+            }  
+            else
+                throw new ArgumentException("the email is not excist");
+
+        }
+        public User register(string email, string password) 
+        {
+            if (validateEmail(email) == true & validatePasswordRules(password))
+            {
+                User newUser = new User(email,password);
+                users.Add(email, newUser);
+                return newUser;
+            }
+            else
+                throw new ArgumentException("the email or the password is invalid");
+        }
+        private bool validateEmail(string email)
+        {
+            return !(users.ContainsKey(email) == true);
+        }
+        private bool validatePasswordRules(string password) 
+        {
+            bool foundUpper = false;
+            bool foundNumber = false;
+            bool foundSmall = false;
+
+            if (!(password.Length >= 4 & password.Length <= 20))
+                return false;
+            for (int i = 0; i <= password.Length | (!foundSmall & !foundNumber & !foundUpper); i++)
+            {
+                if (Char.IsUpper(password[i]))
+                    foundUpper = true;
+                if (Char.IsDigit(password[i]))
+                    foundNumber = true;
+                if (Char.IsLetter(password[i]))
+                    foundSmall = true;
+            }
+
+            if (foundSmall & foundNumber & foundUpper)
+                return true;
+            return false;
+        }
 
     }
 }
