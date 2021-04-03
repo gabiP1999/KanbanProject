392dbdd3a8e8d8a4545b1589a8a11e9a773374ae
shirMaximof <maximof@post.bgu.ac.il>
2021-04-03 13:58:40 +0300

    adding Column

--- Backend/BusinessLayer/Column.cs
+++ Backend/BusinessLayer/Column.cs
@@ -11,12 +11,40 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         private string name;
         private int maxTasks;
         private Dictionary<string,Task> Tasks;
-        public Column(string name) { }
-        public Column(string name, int maxTasks) { }
-        public void setMaxTasks(int maxTasks) { }
-        public bool isBelong(Task task) { }
-        public bool addTask(Task task) { }
-        public Task removeTask(string id) { }
+        public Column(string name) {
+            this.name = name;
+            Tasks = new Dictionary<string, Task>();
+        
+        }
+        public Column(string name, int maxTasks) {
+            this.name = name;
+            this.maxTasks = maxTasks;
+            Tasks = new Dictionary<string, Task>();
+        }
+        public void setMaxTasks(int maxTasks) {
+            this.maxTasks = maxTasks;
+        }
+        public bool isBelong(Task task) {
+            return Tasks.ContainsKey(task.getID());
+        }
+        public bool addTask(Task task) {
+            bool added = Tasks.ContainsKey(task.getID());
+            if(added)
+                 Tasks[task.getID()] = task;
+            return added;
+        }
+        public Task removeTask(Task task) {
 
+            if (Tasks.ContainsKey(task.getID()))
+                this.Tasks.Remove(task.getID());
+            else
+                Console.WriteLine("id is not found");
+            return task;
+        
+        }
+
+        public string getName() {
+            return this.name;
+        }
     }
 }
